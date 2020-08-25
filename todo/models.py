0ccb91783451de83026683692ccba3bbfdfcec35
Juan Felipe Castro <JuanFelipe.Castro@endava.com>
2020-08-25 17:29:16 -0500

    added model and migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=110)
+    description = models.TextField(blank=True)
+    important = models.BooleanField(default=False, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    completed_date = models.DateTimeField(blank=True, null=True)
+
